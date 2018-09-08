62532d918c71f03b95b183642b99ee6111302874
pythonlectures <marcoverduci@yahoo.com>
2018-09-08 15:47:42 +0200

    variant for tests. still not sure when to use fixtures and when to aggregate asserts in one function

--- battleship_oo2_test.py
+++ battleship_oo2_test.py
@@ -39,24 +39,34 @@ def a_missing_shot():
     return btl.Shot(btl.Coordinates(4, 4))
 
 
+@pytest.fixture
+def ship_is_hit():
+    ship = a_ship_half_hit()
+    ship.is_hit(shot=a_hitting_shot())
+    return ship
+
+
+@pytest.fixture
+def ship_is_not_hit():
+    ship = a_ship_half_hit()
+    ship.is_hit(shot=a_missing_shot())
+    return ship
+
+
 class TestShip(object):
 
-    def test_is_cell_set_to_hit_when_hit(self):
-        ship = a_ship_half_hit()
-        ship.is_hit(shot=a_hitting_shot())
+    def test_is_cell_set_to_hit_when_shot_is_a_hit(self):
+        ship = ship_is_hit()
         assert ship._cells[1].get_status() is btl.CellStatus.HIT
 
-    def test_is_cell_still_undiscovered_when_miss(self):
-        ship = a_ship_half_hit()
-        ship.is_hit(shot=a_missing_shot())
+    def test_is_cell_still_undiscovered_when_shot_is_a_miss(self):
+        ship = ship_is_not_hit()
         assert ship._cells[1].get_status() is btl.CellStatus.UNDISCOVERED
 
-    def test_is_sunk_when_hit(self):
-        ship = a_ship_half_hit()
-        ship.is_hit(shot=a_hitting_shot())
+    def test_is_sunk_when_shot_is_a_hit(self):
+        ship = ship_is_hit()
         assert ship._is_sunk is True
 
-    def test_is_sunk_when_miss(self):
-        ship = a_ship_half_hit()
-        ship.is_hit(shot=a_missing_shot())
+    def test_is_sunk_when_shot_is_a_miss(self):
+        ship = ship_is_not_hit()
         assert ship._is_sunk is False
